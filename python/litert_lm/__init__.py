ac8271948a03c90d499dd37c04120dd8149b9b68
Changming Sun <me@cpp.io>
2026-04-02 18:20:08 -0700

    update

--- python/litert_lm/__init__.py
+++ python/litert_lm/__init__.py
@@ -18,7 +18,6 @@ from .interfaces import AbstractBenchmark
 from .interfaces import AbstractConversation
 from .interfaces import AbstractEngine
 from .interfaces import AbstractSession
-from .interfaces import AbstractTokenizer
 from .interfaces import Backend
 from .interfaces import BenchmarkInfo
 from .interfaces import DecodeOptions
@@ -33,7 +32,6 @@ from .litert_lm_ext import Conversation  # pytype: disable=import-error
 from .litert_lm_ext import Engine  # pytype: disable=import-error
 from .litert_lm_ext import LogSeverity  # pytype: disable=import-error
 from .litert_lm_ext import Session  # pytype: disable=import-error
-from .litert_lm_ext import Tokenizer  # pytype: disable=import-error
 from .litert_lm_ext import set_min_log_severity  # pytype: disable=import-error
 from .tools import Tool
 from .tools import tool_from_function
@@ -48,14 +46,12 @@ AbstractConversation.register(Conversation)
 AbstractBenchmark.register(_Benchmark)
 BenchmarkInfo.register(_BenchmarkInfo)
 AbstractSession.register(Session)
-AbstractTokenizer.register(Tokenizer)
 
 __all__ = (
     "AbstractBenchmark",
     "AbstractConversation",
     "AbstractEngine",
     "AbstractSession",
-    "AbstractTokenizer",
     "Backend",
     "Benchmark",
     "BenchmarkInfo",
@@ -68,7 +64,6 @@ __all__ = (
     "SessionOptions",
     "Tool",
     "ToolEventHandler",
-    "Tokenizer",
     "set_min_log_severity",
     "tool_from_function",
 )
